b7d76c8d797eead82a88478ada13b4fd95919a34
Khingz <kingsley995mr@gmail.com>
2024-01-15 19:56:16 +0000

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Simple Async await"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """Simple async function"""
+    time = random.uniform(max_delay)
+    await asyncio.sleep(time)
+    return (time)
