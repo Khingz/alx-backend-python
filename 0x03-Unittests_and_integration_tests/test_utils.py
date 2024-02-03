d07612299da386ff8fd04fdff46febf6d964d8aa
khingz <kingsley995mr@gmail.com>
2024-02-03 15:20:32 +0000

    parametize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,11 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
-    def test_access_nested_map(self, in_map, path, expected):
+    def test_access_nested_map(
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
         """Connemt
         """
         self.assertEqual(access_nested_map(in_map, path), expected)
