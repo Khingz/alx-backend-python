739928eac911e6e185036cf38a6630049678fe54
khingz <kingsley995mr@gmail.com>
2024-02-03 15:16:18 +0000

    task 0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/bib/usr/env python3
+"""Test package for utils class
+"""
+from utils import access_nested_map
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """test class for utils, access_nested method
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a"), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_access_nested_map(self, in_map, path, expected):
+        """Connemt
+        """
+        self.assertEqual(access_nested_map(in_map, path), expected)
